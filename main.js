d5eaaf622b365fd2c19ab90efdc53bdc26e1cc9a
Arawn Park <arawn.kr@gmail.com>
2024-01-21 09:39:12 +0900

    Use todomvc-app-template for todos.html

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+
+const htmls = [
+  'todos.html',
+];
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <h1>Welcome. This is a Todoapp Client Project!</h1>
+    <p>Pages</p>
+    <ul class="pages">
+      ${htmls.map(file => `<li><a href="/pages/${file}">${file}</a></li>`).join('')}
+    </ul>
   </div>
 `
-
-setupCounter(document.querySelector('#counter'))
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
-export default {
-    build: {
-        target: 'modules',
-        minify: false,
-    },
-}
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    target: 'modules',
+    minify: false,
+    rollupOptions: {
+      input: {
+        root: resolve(__dirname, 'index.html'),
+        todos: resolve(__dirname, 'pages/todos.html'),
+      },
+    }
+  },
+})
